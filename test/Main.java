d4e9e9bdd0d227982157416b64ba21434aff52a9
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-10-24 23:09:44 +0600

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -1,7 +1,16 @@
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
 public class Main {
 
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
-        new ImageCanvas();
+        JFrame frame = new JFrame("Canvas Game");
+        Canvas cvs = new ImageCanvas();
+        frame.add(cvs);
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
