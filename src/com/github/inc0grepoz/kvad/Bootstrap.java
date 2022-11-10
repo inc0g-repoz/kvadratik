3096dbc08b5ae69933cceaeff891c4e18f667a5c
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-10 19:00:04 +0600

    Add some launch args

--- src/com/github/inc0grepoz/kvad/Bootstrap.java
+++ src/com/github/inc0grepoz/kvad/Bootstrap.java
@@ -1,5 +1,7 @@
 package com.github.inc0grepoz.kvad;
 
+import com.github.inc0grepoz.kvad.client.KvadratikClient;
+
 public class Bootstrap {
 
     public static void main(String[] args) {
@@ -8,6 +10,27 @@ public class Bootstrap {
         game.setResizable(false);
         game.setLocationRelativeTo(null);
         game.setVisible(true);
+
+        KvadratikClient client = game.getClient();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].contains("=")) {
+                String[] split = args[i].split("=");
+                switch (split[0]) {
+                    case "nickname": {
+                        client.setNickname(split[1]);
+                        break;
+                    }
+                    case "server": {
+                        String[] server = split[1].split(":");
+                        client.setServerIp(server[0]);
+                        client.setServerPort(Integer.valueOf(server[1]));
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
 }
