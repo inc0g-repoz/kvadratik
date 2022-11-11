7c30dcb97b4dca46a9c6d0aee700e3a4b85afd8c
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-11 15:52:15 +0600

    Update Packet.java

--- src/com/github/inc0grepoz/kvad/protocol/Packet.java
+++ src/com/github/inc0grepoz/kvad/protocol/Packet.java
@@ -8,11 +8,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.IntStream.Builder;
+import java.util.stream.Stream;
 
 import com.github.inc0grepoz.kvad.client.KvadratikClient;
+import com.github.inc0grepoz.kvad.entities.being.Being;
+import com.github.inc0grepoz.kvad.entities.being.BeingType;
+import com.github.inc0grepoz.kvad.entities.level.Level;
+import com.github.inc0grepoz.kvad.utils.Logger;
 
 public class Packet {
 
@@ -74,6 +83,46 @@ public class Packet {
 
     public void send(OutputStream out) throws IOException {
         out.write(data, 0, data.length);
+    }
+
+    public void createBeing(Level level) {
+        if (type != PacketType.SERVER_BEING_SPAWN) {
+            Logger.error("Unable to init a being from " + type.name());
+        }
+
+        Map<String, String> map = toMap();
+
+        // Checking if the level is an appropriate one
+        String srvLevel = map.get("level");
+        if (!level.getName().equals(srvLevel)) {
+            return;
+        }
+
+        // Building a rectangle array
+        Builder b = IntStream.builder();
+        Stream.of(map.get("rect").split(",")).forEach(s -> {
+            b.add(Integer.valueOf(s).intValue());
+        });
+
+        // Looking for the client-side being type
+        BeingType type = BeingType.valueOf(map.get("type"));
+
+        Being being = new Being(b.build().toArray(), level, type);
+        level.getBeings().add(being);
+    }
+
+    private Map<String, String> toMap() {
+        Map<String, String> map = new HashMap<>();
+        String[] content = string.contains(";") ? string.split(";") : new String[] { string };
+
+        for (int i = 0; i < content.length; i++) {
+            if (content[i].contains("=")) {
+                String[] kvp = content[i].split("=");
+                map.put(kvp[0], kvp[1]);
+            }
+        }
+
+        return map;
     }
 
 }
