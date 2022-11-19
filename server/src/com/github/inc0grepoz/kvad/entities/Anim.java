5ae2a709cd55c79087cc7530c40b1297da99c69f
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-19 14:40:17 +0600

    Update Anim.java

--- server/src/com/github/inc0grepoz/kvad/entities/Anim.java
+++ server/src/com/github/inc0grepoz/kvad/entities/Anim.java
@@ -2,21 +2,40 @@ package com.github.inc0grepoz.kvad.entities;
 
 public enum Anim {
 
-    HIT_W,
-    HIT_A,
-    HIT_S,
-    HIT_D,
-    IDLE_W,
-    IDLE_A,
-    IDLE_S,
-    IDLE_D,
-    RUN_W,
-    RUN_A,
-    RUN_S,
-    RUN_D,
-    WALK_W,
-    WALK_A,
-    WALK_S,
-    WALK_D;
+    HIT_W  (Way.W),
+    HIT_A  (Way.A),
+    HIT_S  (Way.S),
+    HIT_D  (Way.D),
+    IDLE_W (Way.W),
+    IDLE_A (Way.A),
+    IDLE_S (Way.S),
+    IDLE_D (Way.D),
+    RUN_W  (Way.W),
+    RUN_A  (Way.A),
+    RUN_S  (Way.S),
+    RUN_D  (Way.D),
+    WALK_W (Way.W),
+    WALK_A (Way.A),
+    WALK_S (Way.S),
+    WALK_D (Way.D);
+
+    public static enum Way {
+
+        W(0, -1), A(-1, 0), S(0, 1), D(1, 0);
+
+        public final int x, y;
+
+        Way(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    public final Way way;
+
+    Anim(Way way) {
+        this.way = way;
+    }
 
 }
