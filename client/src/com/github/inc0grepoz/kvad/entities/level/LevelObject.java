64daf0ed01f0c52bcca29497e7147e8138e8a0e1
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-19 14:39:49 +0600

    Update LevelObject.java

--- client/src/com/github/inc0grepoz/kvad/entities/level/LevelObject.java
+++ client/src/com/github/inc0grepoz/kvad/entities/level/LevelObject.java
@@ -4,6 +4,10 @@ import com.github.inc0grepoz.kvad.entities.Renderable;
 
 public abstract class LevelObject extends Renderable {
 
+    public LevelObject(Level level, int[] rect, int[] coll) {
+        super(level, rect, coll);
+    }
+
     public LevelObject(Level level, int[] rect) {
         super(level, rect);
     }
