da3063e777690dc92e2e573a016b27a346899b11
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-19 14:39:57 +0600

    Update Vector.java

--- client/src/com/github/inc0grepoz/kvad/utils/Vector.java
+++ client/src/com/github/inc0grepoz/kvad/utils/Vector.java
@@ -4,4 +4,32 @@ public class Vector {
 
     public int x, y;
 
+    public Vector() {}
+
+    public Vector(int x, int y) {
+        set(x, y);
+    }
+
+    public Vector set(int x, int y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Vector multiply(int by) {
+        x *= by;
+        y *= by;
+        return this;
+    }
+
+    public Vector divide(int by) {
+        x /= by;
+        y /= by;
+        return this;
+    }
+
+    public Vector clone() {
+        return new Vector(x, y);
+    }
+
 }
