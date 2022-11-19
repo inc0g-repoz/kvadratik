66f10c2affa8631a85a02591dfc40eef69712a30
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-19 14:39:34 +0600

    Update ControlsHandler.java

--- client/src/com/github/inc0grepoz/kvad/ControlsHandler.java
+++ client/src/com/github/inc0grepoz/kvad/ControlsHandler.java
@@ -4,18 +4,28 @@ import com.github.inc0grepoz.kvad.Controls.Key;
 
 public class ControlsHandler {
 
-//  private final Controls controls;
+    private final Controls controls;
 
     public ControlsHandler(Controls controls) {
-//      this.controls = controls;
+        this.controls = controls;
     }
 
     public void onKeyPressed(Key key) {
-        
+        switch (key) {
+            case SPRINT:
+                controls.getGame().getLevel().getPlayer().sprint = true;
+                break;
+            default:
+        }
     }
 
     public void onKeyReleased(Key key) {
-        
+        switch (key) {
+            case SPRINT:
+                controls.getGame().getLevel().getPlayer().sprint = false;
+                break;
+            default:
+        }
     }
 
 }
