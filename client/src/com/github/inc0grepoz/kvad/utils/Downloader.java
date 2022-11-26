47dbfa37cb7e9ff719139dcff21ecee64fce4ef5
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-11-26 21:40:31 +0600

    Create Downloader.java

---
+++ client/src/com/github/inc0grepoz/kvad/utils/Downloader.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 Daniil Nikonov (inc0g-repoz)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.inc0grepoz.kvad.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class Downloader {
+
+    public static void download(URL url, File filePath) {
+        try {
+            Path dest = filePath.toPath();
+            try (InputStream inputStream = url.openStream()) {
+                Files.copy(inputStream, dest, StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static int getFileSize(URL url) {
+        URLConnection conn = null;
+        try {
+            conn = url.openConnection();
+            if (conn instanceof HttpURLConnection) {
+                ((HttpURLConnection) conn).setRequestMethod("HEAD");
+            }
+            conn.getInputStream();
+            return conn.getContentLength();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn instanceof HttpURLConnection) {
+                ((HttpURLConnection) conn).disconnect();
+            }
+        }
+    }
+
+}
