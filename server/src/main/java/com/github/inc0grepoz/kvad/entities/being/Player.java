c71f88e3742672c33784c80623ff74185527ced1
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2022-12-29 01:14:25 +0600

    Replace 3 more getters with lombok annotations

--- server/src/main/java/com/github/inc0grepoz/kvad/entities/being/Player.java
+++ server/src/main/java/com/github/inc0grepoz/kvad/entities/being/Player.java
@@ -4,23 +4,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 
 import com.github.inc0grepoz.kvad.entities.Connection;
 import com.github.inc0grepoz.kvad.entities.level.Level;
-import com.github.inc0grepoz.kvad.protocol.Packet;
 import com.github.inc0grepoz.kvad.utils.RGB;
 import com.github.inc0grepoz.kvad.utils.Vector;
 
+import lombok.Getter;
+
 public class Player extends Being {
 
-    private final Connection connection;
-    private final String name;
-    private final Color chatColor = RGB.random();
-    private final Queue<Packet> queuedPackets = new LinkedList<>();
+    private final @Getter Connection connection;
+    private final @Getter String name;
+    private final @Getter Color chatColor = RGB.random();
 
     public Player(Connection connection, String name, Level level, Rectangle rect,
             Dimension collSize, Vector collOffset, String type) {
@@ -29,37 +26,10 @@ public class Player extends Being {
         this.name = name;
     }
 
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Color getChatColor() {
-        return chatColor;
-    }
-
     public void disconnect() {
         try {
             connection.close();
         } catch (IOException e) {
-        }
-    }
-
-    public void queue(Packet packet) {
-        queuedPackets.add(packet);
-    }
-
-    public void flushQueuedPackets() throws IOException {
-        OutputStream out = connection.getOutputStream();
-
-        synchronized (queuedPackets) {
-            for (Packet packet : queuedPackets) {
-                packet.send(out);
-            }
-            queuedPackets.clear();
         }
     }
 
